ea84716962e75d6403d8555a6e644da4c654852c
Johannes Schindelin <johannes.schindelin@gmx.de>
2026-01-14 13:11:18 +0100

    Adjust to build without a signing key
    This is a lot harder than it looks at first: Simply removing the
    `pubkey` attribute from `tauri.conf.json` would run into this problem:
    
      Error failed to build bundler settings: failed to parse updater plugin
      configuration: missing field `pubkey`
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -368,7 +368,6 @@ pub fn run() {
         }))
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_macos_permissions::init())
